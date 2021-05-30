505303863baf0cf511acfcb7ddaedef78bcb08c7
guyleaf <ychhua1@gmail.com>
2021-05-30 21:50:19 +0800

    Modify entrypoint.sh

--- standalone/entrypoint.sh
+++ standalone/entrypoint.sh
@@ -3,7 +3,13 @@
 set -euo pipefail
 
 export PATH="$PATH:/hbase/bin"
+export HBASE_HOME="/hbase"
 
 bash start-hbase.sh
 
 jps
+
+trap 'trap - INT; kill "$!"; exit' INT
+exec tail -f /dev/null "$HBASE_HOME/logs/"* & wait $!
+
+bash stop-hbase.sh
